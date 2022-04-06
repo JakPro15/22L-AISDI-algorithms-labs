db8ad4d2e8f8a2c795ba46df43e9d86e6a7771ef
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2022-04-06 20:12:51 +0200

    Implemented and tested the AVL tree

--- AISDI-22L-3-Drzewa/avl_tree.py
+++ AISDI-22L-3-Drzewa/avl_tree.py
@@ -3,24 +3,24 @@ from bstree import Binary_Search_Tree_Node
 
 class AVL_Tree:
     def __init__(self, values=None):
-        self.root = AVL_Tree_Node(self)
+        self.root = AVL_Tree_Node(self, None, None)
         if values:
             for value in values:
                 self.root.insert(value)
-    
+
     def insert(self, value):
         self.root.insert(value)
-    
+
     @staticmethod
     def node_to_string(result_lines, node, depth):
         if len(result_lines) <= depth:
             result_lines.append("")
         if node.value:
             result_lines[depth] += f"{node.value} "
-        if node.left_tree:
-            AVL_Tree.node_to_string(result_lines, node.left_tree, depth + 1)
-        if node.right_tree:
-            AVL_Tree.node_to_string(result_lines, node.right_tree, depth + 1)
+        if node.left_child:
+            AVL_Tree.node_to_string(result_lines, node.left_child, depth + 1)
+        if node.right_child:
+            AVL_Tree.node_to_string(result_lines, node.right_child, depth + 1)
 
     def to_string(self):
         result_lines = []
@@ -28,73 +28,95 @@ class AVL_Tree:
         return "\n".join(result_lines)
 
 
-class AVL_Tree_Node(Binary_Search_Tree_Node):
-    def __init__(self, parent, value=None):
-        super().__init__(value)
+class AVL_Tree_Node:
+    def __init__(self, parent, right: bool = None, value=None):
+        self.left_child = None
+        self.right_child = None
+        self.value = value
         self.parent = parent
         self.balance_factor = 0
+        if right is not None:
+            if right:
+                self.parent.right_child = self
+            else:
+                self.parent.left_child = self
+
+        # AVL Rebalancing
+        # Fix balance factors if value was just inserted here
+        if self.value is not None:
+            a = self
+            while isinstance(a.parent, AVL_Tree_Node):
+                # Balance factors
+                if a == a.parent.right_child:
+                    a.parent.balance_factor += 1
+                else:
+                    a.parent.balance_factor -= 1
+
+                # Rotations
+                if a.parent.balance_factor == 2:
+                    if a.balance_factor > 0:
+                        # Right-Right case - Left rotation needed
+                        a._rotate_left()
+                    else:
+                        # Right-Left case - Right-Left rotation needed
+                        a._rotate_right_left()
+                    break
+                elif a.parent.balance_factor == -2:
+                    if a.balance_factor < 0:
+                        # Left-Left case - Right rotation needed
+                        a._rotate_right()
+                    else:
+                        # Left-Right case - Left-Right rotation needed
+                        a._rotate_left_right()
+                    break
+                elif a.parent.balance_factor == 0:
+                    break
+
+                # Move to parent
+                if isinstance(a.parent, AVL_Tree_Node):
+                    a = a.parent
 
     def insert(self, value):
         # Insert like in BST
-        if not self.value:
+        # AVL rebalancing done in constructor of the new node
+        if self.value is None:
             self.value = value
         elif self.value == value:
             return
         elif value < self.value:
-            if self.left_tree:
-                self.left_tree.insert(value)
+            if self.left_child:
+                self.left_child.insert(value)
             else:
-                self.left_tree = AVL_Tree_Node(self, value)
-        elif self.right_tree:
-            self.right_tree.insert(value)
+                AVL_Tree_Node(self, False, value)
+        elif self.right_child:
+            self.right_child.insert(value)
         else:
-            self.right_tree = AVL_Tree_Node(self, value)
-        
-        if isinstance(self.parent, AVL_Tree_Node):
-            # Fix balance factor
-            if self == self.parent.right_tree:
-                self.parent.balance_factor += 1
-            else:
-                self.parent.balance_factor -= 1
-        
-            if self.parent.balance_factor == 2:
-                if self.balance_factor > 0:
-                    # Right-Right case - Left rotation needed
-                    self._rotate_left()
-                else:
-                    # Right-Left case - Right-Left rotation needed
-                    self._rotate_right_left()
-            elif self.parent.balance_factor == -2:
-                if self.balance_factor < 0:
-                    # Left-Left case - Right rotation needed
-                    self._rotate_right()
-                else:
-                    # Left-Right case - Left-Right rotation needed
-                    self._rotate_left_right()
+            AVL_Tree_Node(self, True, value)
+
 
     def _rotate_left(self, set_factors=True):
         x = self.parent
         # Rebind the left child of self
-        if self.left_tree:
-            x.right_tree = self.left_tree
-            self.left_tree.parent = x
+        if self.left_child:
+            x.right_child = self.left_child
+            self.left_child.parent = x
         else:
-            x.right_tree = None
+            x.right_child = None
 
         # Rotate self with X
         if isinstance(x.parent, AVL_Tree_Node):
-            if x == x.parent.left_tree:
-                x.parent.left_tree = self
+            if x == x.parent.left_child:
+                x.parent.left_child = self
             else:
-                x.parent.right_tree = self
+                x.parent.right_child = self
             self.parent = x.parent
             x.parent = self
-            self.left_tree = x
+            self.left_child = x
         else:
             x.parent.root = self
             self.parent = x.parent
             x.parent = self
-            self.left_tree = x
+            self.left_child = x
 
         if set_factors:
             # Fix balance factors
@@ -109,26 +131,26 @@ class AVL_Tree_Node(Binary_Search_Tree_Node):
     def _rotate_right(self, set_factors=True):
         x = self.parent
         # Rebind the right child of self
-        if self.right_tree:
-            x.left_tree = self.right_tree
-            self.right_tree.parent = x
+        if self.right_child:
+            x.left_child = self.right_child
+            self.right_child.parent = x
         else:
-            x.left_tree = None
+            x.left_child = None
 
         # Rotate self with X
         if isinstance(x.parent, AVL_Tree_Node):
-            if x == x.parent.left_tree:
-                x.parent.left_tree = self
+            if x == x.parent.left_child:
+                x.parent.left_child = self
             else:
-                x.parent.right_tree = self
+                x.parent.right_child = self
             self.parent = x.parent
             x.parent = self
-            self.right_tree = x
+            self.right_child = x
         else:
             x.parent.root = self
             self.parent = x.parent
             x.parent = self
-            self.right_tree = x
+            self.right_child = x
 
         if set_factors:
             # Fix balance factors
@@ -141,7 +163,7 @@ class AVL_Tree_Node(Binary_Search_Tree_Node):
 
     def _rotate_left_right(self):
         x = self.parent
-        y = self.right_tree
+        y = self.right_child
 
         # First rotate Y left...
         y._rotate_left(False)
@@ -153,15 +175,16 @@ class AVL_Tree_Node(Binary_Search_Tree_Node):
             x.balance_factor = 0
             self.balance_factor = 0
         elif y.balance_factor > 0:
-            x.balance_factor = 1
-            self.balance_factor = 0
-        else:
             x.balance_factor = 0
             self.balance_factor = -1
+        else:
+            x.balance_factor = 1
+            self.balance_factor = 0
+        y.balance_factor = 0
 
     def _rotate_right_left(self):
         x = self.parent
-        y = self.left_tree
+        y = self.left_child
 
         # First rotate Y right...
         y._rotate_right(False)
@@ -178,3 +201,4 @@ class AVL_Tree_Node(Binary_Search_Tree_Node):
         else:
             x.balance_factor = 0
             self.balance_factor = 1
+        y.balance_factor = 0
--- AISDI-22L-3-Drzewa/test_avl_tree.py
+++ AISDI-22L-3-Drzewa/test_avl_tree.py
@@ -1,19 +1,189 @@
 from avl_tree import AVL_Tree
+from random import randint, gauss
 
 
-def test_1():
+def get_subtree_height(node):
+    if node.left_child and node.right_child:
+        tree_height = max(
+            get_subtree_height(node.left_child),
+            get_subtree_height(node.right_child)
+        )
+    elif node.left_child:
+        tree_height = get_subtree_height(node.left_child)
+    elif node.right_child:
+        tree_height = get_subtree_height(node.right_child)
+    else:
+        tree_height = 0
+    return tree_height + 1
+
+
+def go_down_the_tree(node, max, min):
+    if node.left_child:
+        left_height = get_subtree_height(node.left_child)
+    else:
+        left_height = 0
+
+    if node.right_child:
+        right_height = get_subtree_height(node.right_child)
+    else:
+        right_height = 0
+
+    assert node.balance_factor == right_height - left_height
+    if max:
+        assert node.value < max
+    if min:
+        assert node.value > min
+    if node.left_child:
+        go_down_the_tree(node.left_child, min=min, max=node.value)
+    if node.right_child:
+        go_down_the_tree(node.right_child, min=node.value, max=max)
+
+
+def test_basic_avl_tree_1():
+    tree = AVL_Tree([1])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 1
+    assert tree.root.left_child is None
+    assert tree.root.right_child is None
+    assert tree.root.balance_factor == 0
+
+
+def test_basic_avl_tree_2():
+    tree = AVL_Tree([1, 0])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 1
+    assert tree.root.right_child is None
+    assert tree.root.balance_factor == -1
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == 0
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+
+def test_basic_avl_tree_3():
+    tree = AVL_Tree([1, 0, 2])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 1
+    assert tree.root.balance_factor == 0
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == 0
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+    assert tree.root.right_child.parent is tree.root
+    assert tree.root.right_child.value == 2
+    assert tree.root.right_child.left_child is None
+    assert tree.root.right_child.right_child is None
+    assert tree.root.right_child.balance_factor == 0
+
+
+def test_basic_avl_tree_right_rotation():
+    # Here a right rotation is needed
+    tree = AVL_Tree([1, 0, -1])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 0
+    assert tree.root.balance_factor == 0
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == -1
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+    assert tree.root.right_child.parent is tree.root
+    assert tree.root.right_child.value == 1
+    assert tree.root.right_child.left_child is None
+    assert tree.root.right_child.right_child is None
+    assert tree.root.right_child.balance_factor == 0
+
+
+def test_basic_avl_tree_left_rotation():
+    # Here a left rotation is needed
+    tree = AVL_Tree([-1, 0, 1])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 0
+    assert tree.root.balance_factor == 0
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == -1
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+    assert tree.root.right_child.parent is tree.root
+    assert tree.root.right_child.value == 1
+    assert tree.root.right_child.left_child is None
+    assert tree.root.right_child.right_child is None
+    assert tree.root.right_child.balance_factor == 0
+
+
+def test_basic_avl_tree_left_right_rotation():
+    # Here a right-left rotation is needed
+    tree = AVL_Tree([1, -1, 0])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 0
+    assert tree.root.balance_factor == 0
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == -1
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+    assert tree.root.right_child.parent is tree.root
+    assert tree.root.right_child.value == 1
+    assert tree.root.right_child.left_child is None
+    assert tree.root.right_child.right_child is None
+    assert tree.root.right_child.balance_factor == 0
+
+
+def test_basic_avl_tree_right_left_rotation():
+    # Here a right-left rotation is needed
+    tree = AVL_Tree([-1, 1, 0])
+    go_down_the_tree(tree.root, None, None)
+    assert tree.root.parent is tree
+    assert tree.root.value == 0
+    assert tree.root.balance_factor == 0
+
+    assert tree.root.left_child.parent is tree.root
+    assert tree.root.left_child.value == -1
+    assert tree.root.left_child.left_child is None
+    assert tree.root.left_child.right_child is None
+    assert tree.root.left_child.balance_factor == 0
+
+    assert tree.root.right_child.parent is tree.root
+    assert tree.root.right_child.value == 1
+    assert tree.root.right_child.left_child is None
+    assert tree.root.right_child.right_child is None
+    assert tree.root.right_child.balance_factor == 0
+
+
+def test_sorted_list():
     values = list(range(1000))
     tree = AVL_Tree(values)
 
-    def go_down_the_tree(node, max, min):
-        assert node.balance_factor in {-1, 0, 1}
-        if max:
-            assert node.value < max
-        if min:
-            assert node.value > min
-        if node.left_tree:
-            go_down_the_tree(node.left_tree, min=min, max=node.value)
-        if node.right_tree:
-            go_down_the_tree(node.left_tree, min=node.value, max=max)
-    
+    go_down_the_tree(tree.root, None, None)
+
+
+def test_random_list():
+    values = [randint(0, 1000) for _ in range(1000)]
+    tree = AVL_Tree(values)
+
+    go_down_the_tree(tree.root, None, None)
+
+
+def test_gauss_random_list():
+    values = [gauss(500, 250) for _ in range(1000)]
+    tree = AVL_Tree(values)
+
     go_down_the_tree(tree.root, None, None)
