d9be976bec2490197fd12c065a801b544f2a9f13
Paweł Kochański <pawilioxion@gmail.com>
2022-05-30 18:53:56 +0200

    added printing path

--- AISDI-22L-6-Grafy/main.py
+++ AISDI-22L-6-Grafy/main.py
@@ -1,4 +1,5 @@
 from file_reader import read_file
+from print_path import show_path, print_tiles
 
 
 if __name__ == "__main__":
---
+++ AISDI-22L-6-Grafy/print_path.py
@@ -0,0 +1,13 @@
+def show_path(tiles, path):
+    path_tiles = [[" " for tile in line] for line in tiles]
+    for point in path:
+        path_tiles[point[0]][point[1]] = tiles[point[0]][point[1]]
+    return path_tiles
+
+
+def print_tiles(tiles):
+    for line in tiles:
+        line_str = ""
+        for tile in line:
+            line_str += str(tile)
+        print(line_str)
