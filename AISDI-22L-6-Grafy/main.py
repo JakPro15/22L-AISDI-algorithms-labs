5e35fd093aff75ef900e8dcd3f37b2b4628ed0a1
Paweł Kochański <pawilioxion@gmail.com>
2022-05-30 19:21:08 +0200

    showing results all prepared

--- AISDI-22L-6-Grafy/main.py
+++ AISDI-22L-6-Grafy/main.py
@@ -3,4 +3,29 @@ from print_path import show_path, print_tiles
 
 
 if __name__ == "__main__":
+    print("Graph 1\n")
     tiles = read_file("graf1.txt")
+    path = []
+    path_tiles = show_path(tiles, path)
+    print_tiles(tiles)
+    print('\n')
+    print("Path 1\n")
+    print_tiles(path_tiles)
+    print('\n')
+    print("Graph 2\n")
+    tiles = read_file("graf2.txt")
+    path = []
+    path_tiles = show_path(tiles, path)
+    print_tiles(tiles)
+    print('\n')
+    print("Path 2\n")
+    print_tiles(path_tiles)
+    print('\n')
+    print("Graph 3\n")
+    tiles = read_file("graf3.txt")
+    path = []
+    path_tiles = show_path(tiles, path)
+    print_tiles(tiles)
+    print('\n')
+    print("Path 3\n")
+    print_tiles(path_tiles)
